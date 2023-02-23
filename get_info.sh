124002b039a693071a6b76268a6e59cfbe70f3e8
Transistor427 <427departament@gmail.com>
2023-02-23 14:46:51 +0300

    Update get_info.sh

--- get_info.sh
+++ get_info.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 host=$(whoami)
+board=$(uname -n)
+os=$(uname -o)
+kernel_rls=$(uname -o)
 
 if [ -f /home/$host/klipper_config/printer.cfg ];
     then
@@ -9,10 +12,13 @@ if [ -f /home/$host/klipper_config/printer.cfg ];
         prt_conf=($(find /home/$host/ -name printer.cfg))
 fi
 
-ID=($(cat $prt_conf | grep "ZBS" | cut -b 8-16))
-echo "Get S/N."
-
 echo -n > /home/$host/Printer_info/info.txt
-echo "Serial Numder: $ID" >> /home/$host/Printer_info/info.txt
+echo "Get Hostname printer."
+echo "Hostname: $host" >> /home/$host/Printer_info/info.txt
 
-
+ID=($(cat $prt_conf | grep "ZBS" | cut -b 8-16))
+echo "S/N: $ID" >> /home/$host/Printer_info/info.txt
+echo "User: $host" >> /home/$host/Printer_info/info.txt
+echo "Board: $board" >> /home/$host/Printer_info/info.txt
+echo "OS: $os" >> /home/$host/Printer_info/info.txt
+echo "Kernel: $kernel_rls" >> /home/$host/Printer_info/info.txt
