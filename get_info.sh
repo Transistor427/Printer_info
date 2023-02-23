bff20f74f79b13cf26f9bebb353a53de9faf7442
Transistor427 <427departament@gmail.com>
2023-02-23 14:35:27 +0300

    Update get_info.sh

--- get_info.sh
+++ get_info.sh
@@ -12,6 +12,7 @@ fi
 ID=($(cat $prt_conf | grep "ZBS" | cut -b 8-16))
 echo "Get S/N."
 
-echo "Serial Numder: $ID" >> info.txt
+echo -n > /home/$host/Printer_info/info.txt
+echo "Serial Numder: $ID" >> /home/$host/Printer_info/info.txt
 
 
