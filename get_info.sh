9f1d703fee059823a8a8c7e1999b714ebc7c37d0
Transistor427 <427departament@gmail.com>
2023-02-23 14:33:09 +0300

    Update get_info.sh

--- get_info.sh
+++ get_info.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-log_work=1
 
 host=$(whoami)
 
 if [ -f /home/$host/klipper_config/printer.cfg ];
     then
-        klp_conf=/home/$host/klipper_config/printer.cfg
+        prt_conf=/home/$host/klipper_config/printer.cfg
     else
-        klp_conf=($(find /home/$host/ -name printer.cfg))
+        prt_conf=($(find /home/$host/ -name printer.cfg))
 fi
 
-ID=($(cat $klp_conf | grep "ZBS" | cut -b 8-16))
-if [ $log_work == 1 ]; then echo "Get S/N."
-fi
+ID=($(cat $prt_conf | grep "ZBS" | cut -b 8-16))
+echo "Get S/N."
 
-echo $ID >> info.txt
+echo "Serial Numder: $ID" >> info.txt
+
 
